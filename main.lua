7b85f4868a4d1e34c96238cdbe533b655008f64c
thezudikmaster <remy.192hotmail.com@gmail.com>
2026-07-14 23:55:42 -0600

    Version 0.7.0

---
+++ main.lua
@@ -0,0 +1,654 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Dandy's World Jokers
+--- MOD_ID: dandys_jokers
+--- MOD_AUTHOR: [Thezudik]
+--- MOD_DESCRIPTION: Your favorite toons as Jokers :).
+--- PREFIX: dwjokers
+--- VERSION: 0.7.0
+----------------------------------------------
+------------MOD CODE -------------------------
+
+local card_types = {
+	'Tarot',
+	'Planet',
+	'Spectral'
+}
+
+
+
+to_big = to_big or function(x) return x end
+
+SMODS.Atlas{
+	key = "modicon", 
+	path = "icon.png", 
+	px = 32, 
+	py = 32
+}
+
+SMODS.Atlas{
+    key = 'Jokers', 
+    path = 'Jokers.png', 
+    px = 71, --width of one card
+    py = 95 -- height of one card
+}
+
+SMODS.Joker {
+	key = 'Shrimpo',
+	loc_txt = {
+		name = 'Shrimpo',
+		text = {
+			"{C:mult}+#1#{} Mult",
+			"{C:attention}#2#{} hand size"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = false,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 1,
+	atlas = 'Jokers',
+	pos = { x = 0, y = 0 },
+	cost = 1,
+	config = { extra = { mult = 100, hand_size = -10 } },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult, card.ability.extra.hand_size } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		G.hand:change_size(card.ability.extra.hand_size)
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.hand:change_size(-card.ability.extra.hand_size)
+	end,
+    calculate = function(self, card, context)
+		if context.joker_main then
+			return {
+				mult_mod = card.ability.extra.mult,
+				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+			}
+		end
+	end
+	
+}
+
+SMODS.Joker {
+	key = 'Tisha',
+	loc_txt = {
+		name = 'Tisha',
+		text = {
+			"This Toon gains {C:chips}+#2#{} Chips",
+			"per played hand,",
+			"{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 1,
+	cost = 5,
+	config = { extra = { chips = 0, chip_gain = 10 } },
+	atlas = 'Jokers',
+	pos = { x = 1, y = 0 },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.chips, card.ability.extra.chip_gain } }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			return {
+				chip_mod = card.ability.extra.chips,
+				message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
+			}
+		end
+
+		if context.after then
+			card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_gain
+			return {
+				message = 'Gotta sweep!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+	end
+}
+
+SMODS.Joker {
+	key = 'razzledazzle',
+	loc_txt = {
+		name = 'Razzle n Dazzle',
+		text = {
+			"{C:chips}+#1#{} Chips on odd rounds and",
+			"{C:mult}+#2#{} Mult on even rounds",
+			"{C:inactive}(Currently {C:attention}+#3#{C:attention} #4#{C:inactive})"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 2,
+	cost = 7,
+	config = { extra = { chips = 1000, mult = 100, currently1 = 100, currently2 = "Mult" } },
+	atlas = 'Jokers',
+	pos = { x = 2, y = 0 },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.chips, card.ability.extra.mult, card.ability.extra.currently1, card.ability.extra.currently2, G.GAME.round } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		if G.GAME.round % 2 == 1 then
+				card.ability.extra.currently1 = 1000
+				card.ability.extra.currently2 = "Chips"
+			else
+				card.ability.extra.currently1 = 100
+				card.ability.extra.currently2 = "Mult"
+			end
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			if G.GAME.round % 2 == 1 then
+				card.ability.extra.currently1 = 1000
+				card.ability.extra.currently2 = "Chips"
+				return {
+					chip_mod = card.ability.extra.chips,
+					message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
+				}
+			else
+				card.ability.extra.currently1 = 100
+				card.ability.extra.currently2 = "Mult"
+				return {
+					mult_mod = card.ability.extra.mult,
+					message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+				}
+			end
+		end
+
+		if context.setting_blind then
+			if G.GAME.round % 2 == 1 then
+				card.ability.extra.currently1 = 1000
+				card.ability.extra.currently2 = "Chips"
+				return {
+					message = 'Oh what fun!',
+					colour = G.C.CHIPS
+				}
+			else
+				card.ability.extra.currently1 = 100
+				card.ability.extra.currently2 = "Mult"
+				return {
+					message = 'Oh the misery...',
+					colour = G.C.MULT
+				}
+			end
+		end
+	end
+}
+
+SMODS.Joker {
+	key = 'yatta',
+	loc_txt = {
+		name = 'Yatta',
+		text = {
+			"Grants a random {C:purple}Tarot{},",
+			"{C:blue}Planet{} or {C:spectral}Spectral{} card",
+			"when {C:attention}blind{} is selected",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 1,
+	cost = 5,
+	config = { extra = { set_aux = 'Tarot' } },
+	atlas = 'Jokers',
+	pos = { x = 3, y = 0 },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.set_aux } }
+	end,
+	calculate = function(self, card, context)
+        if context.setting_blind and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+			card.ability.extra.set_aux = pseudorandom_element(card_types, 'dwjokers_yatta')
+            G.E_MANAGER:add_event(Event({
+                func = (function()
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            SMODS.add_card {
+                                set = card.ability.extra.set_aux,
+                            }
+                            G.GAME.consumeable_buffer = 0
+                            return true
+                        end
+                    }))
+                    SMODS.calculate_effect({ message = card.ability.extra.set_aux .. 's FOR EVERYONE!!!', colour = G.C.PURPLE },
+                        context.blueprint_card or card)
+                    return true
+                end)
+            }))
+            return nil, true
+        end
+    end,
+}
+
+SMODS.Joker {
+	key = 'Cosmo',
+	loc_txt = {
+		name = 'Cosmo',
+		text = {
+			"Prevents Death and",
+            "grants {C:blue}+#1#{} extra hands",
+            "{C:red,E:2}self destructs{}",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = false,
+	perishable_compat = false,
+	eternal_compat = false,
+	rarity = 1,
+	cost = 5,
+	config = { extra = { hands = 3 } },
+	atlas = 'Jokers',
+	pos = { x = 1, y = 1 },
+	loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.hands } }
+    end,
+	calculate = function(self, card, context)
+        if G.GAME.current_round.hands_left <= 0 and G.GAME.chips < G.GAME.blind.chips then
+            if context.after and not context.blind_defeated and not context.blueprint then
+                G.E_MANAGER:add_event(Event {
+					blocking = false,
+                    func = function()
+						if not context.after or context.blind_defeated or 
+						G.GAME.current_round.hands_left > 0 or G.GAME.chips >= G.GAME.blind.chips then
+							return true
+						end
+						G.GAME.current_round.hands_left = G.GAME.current_round.hands_left + card.ability.extra.hands
+						play_sound('tarot1')
+                        card:juice_up(0.3, 0.5)
+                        card:start_dissolve()
+                        return true
+                    end
+                })
+				return {
+					message = 'Enjoy, just for you!',
+					colour = G.C.RED
+				}
+            end
+        end
+    end
+}
+
+SMODS.Joker {
+	key = 'Looey',
+	loc_txt = {
+		name = 'Looey',
+		text = {
+			"Gain {C:attention}+#1#{} hand size",
+			"each played hand.",
+			"Resets every round.",
+			"{C:inactive}(Currently {C:chips}+#2#{C:inactive} hand size)"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 1,
+	atlas = 'Jokers',
+	pos = { x = 4, y = 0 },
+	cost = 5,
+	config = { extra = { hand_size = 1, looey_uses = 0 } },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.hand_size, card.ability.extra.looey_uses } }
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.hand:change_size(-card.ability.extra.hand_size * card.ability.extra.looey_uses)
+	end,
+    calculate = function(self, card, context)
+		
+		if context.setting_blind then
+			card.ability.extra.looey_uses = 0
+		end
+
+		if context.after then
+			G.hand:change_size(card.ability.extra.hand_size)
+			card.ability.extra.looey_uses = card.ability.extra.looey_uses + 1
+			return {
+				message = 'Im actually so excited!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+		
+		if context.end_of_round and context.main_eval and not context.blueprint then
+			G.hand:change_size(-card.ability.extra.hand_size * card.ability.extra.looey_uses)
+			card.ability.extra.looey_uses = 0
+			return {
+				message = 'Reset!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+	end
+	
+}
+
+SMODS.Joker {
+	key = 'Brusha',
+	loc_txt = {
+		name = 'Brusha',
+		text = {
+			"When {C:attention}blind{} is selected, gain",
+			"{C:attention}#1#%{} extra discards.",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 1,
+	atlas = 'Jokers',
+	pos = { x = 0, y = 1 },
+	cost = 5,
+	config = { extra = { discards_mult = 25, brusha_uses = 0 } },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.discards_mult, card.ability.extra.brusha_uses } }
+	end,
+	calculate = function(self, card, context)
+		
+		if context.setting_blind then
+			card.ability.extra.brusha_uses = card.ability.extra.brusha_uses + 1
+			ease_discard(math.ceil(G.GAME.round_resets.discards * card.ability.extra.discards_mult / 100))
+			return {
+				message = 'A work of art!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+
+		if context.end_of_round and context.main_eval and not context.blueprint then
+			ease_discard(-math.ceil(G.GAME.round_resets.discards * card.ability.extra.discards_mult / 100) * card.ability.extra.brusha_uses)
+		end
+	end
+}
+
+SMODS.Joker {
+    key = "Toodles",
+	loc_txt = {
+		name = 'Toodles',
+		text = {
+			"Gives {X:mult,C:white} X#1# {} Mult",
+            "for each {C:attention}Lucky Card",
+            "in your {C:attention}full deck",
+            "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+    blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+    rarity = 2,
+    cost = 7,
+	atlas = 'Jokers',
+    pos = { x = 2, y = 1 },
+    config = { extra = { xmult = 0.5 } },
+    loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_lucky
+
+        local lucky_tally = 0
+        if G.playing_cards then
+            for _, playing_card in ipairs(G.playing_cards) do
+                if SMODS.has_enhancement(playing_card, 'm_lucky') then lucky_tally = lucky_tally + 1 end
+            end
+        end
+        return { vars = { card.ability.extra.xmult, 1 + card.ability.extra.xmult * lucky_tally } }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            local lucky_tally = 0
+            for _, playing_card in ipairs(G.playing_cards) do
+                if SMODS.has_enhancement(playing_card, 'm_lucky') then lucky_tally = lucky_tally + 1 end
+            end
+            return {
+                Xmult = 1 + card.ability.extra.xmult * lucky_tally,
+            }
+        end
+    end,
+    in_pool = function(self, args)
+        for _, playing_card in ipairs(G.playing_cards or {}) do
+            if SMODS.has_enhancement(playing_card, 'm_lucky') then
+                return true
+            end
+        end
+        return false
+    end
+}
+
+SMODS.Joker {
+	key = 'Rodger',
+	loc_txt = {
+		name = 'Rodger',
+		text = {
+			"Gain {C:attention}+#1#{} discards for",
+			"each played round.",
+			"{C:inactive}(Currently {C:attention}+#2#{C:inactive} discards)"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = false,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 3, y = 1 },
+	cost = 7,
+	config = { extra = { d_size = 1, d_total = 0} },
+	loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.d_size, card.ability.extra.d_total, G.GAME.round } }
+    end,
+	add_to_deck = function(self, card, from_debuff)
+		card.ability.extra.d_total = G.GAME.round
+		G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.d_total
+		ease_discard(card.ability.extra.d_total)
+	end,
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.discards = G.GAME.round_resets.discards - card.ability.extra.d_total
+        ease_discard(-card.ability.extra.d_total)
+    end,
+	calculate = function(self, card, context)
+        if context.setting_blind and not context.blueprint then
+			card.ability.extra.d_total = G.GAME.round
+			G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.d_size
+			ease_discard(card.ability.extra.d_size)
+			return {
+				message = 'Upgrade!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+    end
+}
+
+SMODS.Joker {
+	key = 'Teagan',
+	loc_txt = {
+		name = 'Teagan',
+		text = {
+			"Doubles all sources",
+			"of {C:money}money gain"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 4, y = 1 },
+	cost = 7,
+	config = { extra = {} },
+	loc_vars = function(self, info_queue, card)
+        return { vars = { G.GAME.dollars } }
+    end,
+	calculate = function(self, card, context)
+		if context.money_altered and to_big(context.amount) > to_big(0) then
+			G.E_MANAGER:add_event(Event {
+				func = function()
+					G.GAME.dollars = G.GAME.dollars + context.amount
+					return true
+				end
+			})
+			return {
+				message = 'Lets fix that!',
+				colour = G.C.MONEY,
+				card = card
+			}
+		end
+    end
+}
+
+SMODS.Joker {
+	key = 'Goob',
+	loc_txt = {
+		name = 'Goob',
+		text = {
+			"Protects all {C:attention}jokers{} in your",
+			"deck from being destroyed.",
+			"{C:inactive}(Itself included)",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = false,
+	eternal_compat = true,
+	rarity = 3,
+	atlas = 'Jokers',
+	pos = { x = 0, y = 2 },
+	cost = 10,
+	calculate = function(self, card, context)
+		if context.joker_type_destroyed then
+			return { no_destroy = true, remove = false, 
+				message = 'Hug time!',
+				colour = G.C.CHIPS,
+				card = card}
+		end
+    end
+}
+
+local original_start_dissolve = Card.start_dissolve
+function Card:start_dissolve(dissolve_colours, silent, dissolve_time_fac, no_juice)
+	local key = self.config.center.key
+	local is_joker = self.ability.set == "Joker"
+	print("test1")
+	if next(SMODS.find_card("j_dwjokers_Goob")) and is_joker then
+		print("exitazo")
+    	return -- Prevent dissolve
+	end
+	local ret = original_start_dissolve(self, dissolve_colours, silent, dissolve_time_fac, no_juice)
+	print("test3")
+    return ret
+end
+
+SMODS.Joker {
+	key = 'Brightney',
+	loc_txt = {
+		name = 'Brightney',
+		text = {
+			"Prevents {C:attention}debuffs{} and",
+			"redirects all {C:attention}destructions{}",
+			"to jokers in your deck to itself.",
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = true,
+	perishable_compat = false,
+	eternal_compat = true,
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 2, y = 2 },
+	cost = 10,
+	calculate = function(self, card, context)
+		if context.joker_type_destroyed and context.main_eval then
+            card:start_dissolve()
+            return { no_destroy = true, remove = false}
+		end
+		if context.debuff_card and context.cardarea == G.jokers then
+			return { prevent_debuff = true, card = context.debuff_card }
+		end
+    end,
+}
+
+SMODS.Joker {
+	key = 'Soulvester',
+	loc_txt = {
+		name = 'Soulvester',
+		text = {
+			"When {C:attention}blind{} is selected,",
+			"Double the {C:attention}hands{} and",
+			"halfs the {C:attention}hand size{}.",
+			"{C:inactive}(Currently {C:attention}#4#{C:inactive})"
+		}
+	},
+	unlocked = true, 
+	discovered = true,
+	blueprint_compat = false,
+	perishable_compat = true,
+	eternal_compat = true,
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 1, y = 2 },
+	cost = 5,
+	config = { extra = { hand_mult = 2, hand_size_mult = 0.5, half_hand_size = 0, soulvester_active_string = 'inactive', soulvester_active = false } },
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.hand_mult, card.ability.extra.hand_size_mult, card.ability.extra.soulvester_active, card.ability.extra.soulvester_active_string } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		card.ability.extra.half_hand_size = math.floor(G.hand.config.card_limit * card.ability.extra.hand_size_mult)
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		if card.ability.extra.soulvester_active then
+			G.hand.config.card_limit = G.hand.config.card_limit / (1 - card.ability.extra.hand_size_mult)
+			G.GAME.current_round.hands_left = G.GAME.current_round.hands_left / card.ability.extra.hand_mult
+		end
+	end,
+    calculate = function(self, card, context)
+		
+		if context.setting_blind then
+			card.ability.extra.soulvester_active_string = 'active'
+			card.ability.extra.soulvester_active = true
+			card.ability.extra.half_hand_size = math.floor(G.hand.config.card_limit * card.ability.extra.hand_size_mult)
+			G.hand.config.card_limit = G.hand.config.card_limit - card.ability.extra.half_hand_size
+			G.GAME.current_round.hands_left = G.GAME.current_round.hands_left * card.ability.extra.hand_mult
+			return {
+				message = 'There is still work to be done.',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+		
+		if context.starting_shop and not context.blueprint then
+			card.ability.extra.soulvester_active_string = 'inactive'
+			card.ability.extra.soulvester_active = false
+			G.hand.config.card_limit = G.hand.config.card_limit + card.ability.extra.half_hand_size
+			G.GAME.current_round.hands_left = G.GAME.current_round.hands_left / card.ability.extra.hand_mult
+			return {
+				message = 'Inactive!',
+				colour = G.C.CHIPS,
+				card = card
+			}
+		end
+	end
+	
+}
+----------------------------------------------
+------------MOD CODE END----------------------
